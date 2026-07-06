211b4e7467b3fa6614fe278e4f4e6e7b6b2c3c8b
G-M-K-75 <gmk152925@gmail.com>
2026-07-06 08:22:05 -0700

    folder creation

---
+++ class.py
@@ -0,0 +1,7 @@
+#input
+first= input("enter your first name: ")
+last = input("enter second name: ")
+D_O_B= int(input("enter your date of birth: "))
+address= input("enter your address: ")
+
+print("your first name is :", first, last, D_O_B, "YEARS OLD.", "your address is ", address)
---
+++ depression_prediction/source/preprocess.py
@@ -0,0 +1,5 @@
+var = "my name its khan g khan's. i'm friom harichand"
+
+print(len(var))  #index.. 0,1,2,3,4
+
+print(var[34:45])
